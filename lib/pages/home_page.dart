6bbf5e3b35517fb48dd09212667af6ee3b64c57d
viraj-lakshitha <virajlakshithabandara@gmail.com>
2021-05-19 23:00:42 +0530

    done: Complete Application Devalopment

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -23,161 +23,163 @@ class _HomePageState extends State<HomePage> {
     String? lastDate = pcrTestData[pcrTestData.length-1].date;
 
     return Scaffold(
-      body: Container(
-        child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            'Covid-19 Updates',
-            style: TextStyle(
-              fontSize: 22.0,
-              fontFamily: 'PTSerif',
-              fontWeight: FontWeight.w600
-            ),
-          ),
-          SizedBox(height: 5.0,),
-          Text(
-            'Source : Health Promotion Bureau - Sri Lanka',
-            style: TextStyle(
-              fontSize: 12.0,
-            ),
-          ),
-          SizedBox(height: 5.0,),
-          Divider(height: 5),
-          SizedBox(height: 50.0,),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(12.0,0,12.0,0),
-            child: Container(
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                  border: Border.all(
-                      color: Colors.black54,
-                      width: 1.0
-                  )
-              ),
-              child: SfCartesianChart(
-                  primaryXAxis: CategoryAxis(),
-                  // Chart title
-                  title: ChartTitle(text: 'PCR Tests\nfrom 2020-02-18 to $lastDate',
-                      textStyle: TextStyle(
-                          fontSize: 12.0,
-                          fontWeight: FontWeight.w600
-                      )
-                  ),
-                  // Enable legend
-                  legend: Legend(isVisible: true),
-                  // Enable tooltip
-                  tooltipBehavior: TooltipBehavior(enable: true),
-                  series: <ChartSeries<PCR, String>>[
-                    LineSeries<PCR, String>(
-                        dataSource: pcrTestData,
-                        xValueMapper: (PCR pcr, _) => pcr.date,
-                        yValueMapper: (PCR pcr, _) => pcr.count,
-                        isVisibleInLegend: false,
-                        // Enable data label
-                        dataLabelSettings: DataLabelSettings(isVisible: false))
-                  ]
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Covid-19 Updates',
+              style: TextStyle(
+                fontSize: 22.0,
+                fontFamily: 'PTSerif',
+                fontWeight: FontWeight.w600
               ),
             ),
-          ),
-          SizedBox(height: 20.0,),
-          // ignore: deprecated_member_use
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  FlatButton(
-                    onPressed: () {
-                      Navigator.pushNamed(context, '/local', arguments: {
-                        'lastUpdateTime' : data['lastUpdateTime'],
-                        'localNewCases' : data['localNewCases'],
-                        'localTotalCase' : data['localTotalCase'],
-                        'localNewDeath' : data['localNewDeath'],
-                        'localDeath' : data['localDeath'],
-                        'localRecovered' : data['localRecovered'],
-                      });
-                    },
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Icon(
-                            Icons.location_on_outlined
-                        ),
-                        Text('Local')
-                      ],
+            SizedBox(height: 5.0,),
+            Text(
+              'Source : Health Promotion Bureau - Sri Lanka',
+              style: TextStyle(
+                fontSize: 12.0,
+              ),
+            ),
+            SizedBox(height: 5.0,),
+            Divider(height: 5),
+            SizedBox(height: 50.0,),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(12.0,0,12.0,0),
+              child: Container(
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                    border: Border.all(
+                        color: Colors.black54,
+                        width: 1.0
+                    )
+                ),
+                child: SfCartesianChart(
+                    primaryXAxis: CategoryAxis(),
+                    // Chart title
+                    title: ChartTitle(text: 'PCR Tests\nfrom 2020-02-18 to $lastDate',
+                        textStyle: TextStyle(
+                            fontSize: 12.0,
+                            fontWeight: FontWeight.w600
+                        )
                     ),
-                    color: Colors.lightBlue[200],
-                    height: 50.0,
-                  ),
-                  SizedBox(width: 15.0,),
-                  // ignore: deprecated_member_use
-                  FlatButton(
-                    onPressed: () {
-                      Navigator.pushNamed(context, '/global', arguments: {
-                        'lastUpdateTime' : data['lastUpdateTime'],
-                        'globalNewCases' : data['globalNewCases'],
-                        'globalTotalCases' : data['globalTotalCases'],
-                        'globalNewDeath' : data['globalNewDeath'],
-                        'globalDeath' : data['globalDeath'],
-                        'globalRecovered' : data['globalRecovered']
-                      });
-                    },
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Icon(
-                            Icons.location_on_outlined
-                        ),
-                        Text('Global')
-                      ],
-                    ),
-                    color: Colors.redAccent[200],
-                    height: 50.0,
-                  ),
-                  SizedBox(width: 15.0,),
-                  FlatButton(
-                    onPressed: () {
-                      Navigator.pushNamed(context, '/info');
-                    },
-                    child: Center(
+                    // Enable legend
+                    legend: Legend(isVisible: true),
+                    // Enable tooltip
+                    tooltipBehavior: TooltipBehavior(enable: true),
+                    series: <ChartSeries<PCR, String>>[
+                      LineSeries<PCR, String>(
+                          dataSource: pcrTestData,
+                          xValueMapper: (PCR pcr, _) => pcr.date,
+                          yValueMapper: (PCR pcr, _) => pcr.count,
+                          isVisibleInLegend: false,
+                          // Enable data label
+                          dataLabelSettings: DataLabelSettings(isVisible: false))
+                    ]
+                ),
+              ),
+            ),
+            SizedBox(height: 20.0,),
+            // ignore: deprecated_member_use
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    FlatButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, '/local', arguments: {
+                          'lastUpdateTime' : data['lastUpdateTime'],
+                          'localNewCases' : data['localNewCases'],
+                          'localTotalCase' : data['localTotalCase'],
+                          'localNewDeath' : data['localNewDeath'],
+                          'localDeath' : data['localDeath'],
+                          'localRecovered' : data['localRecovered'],
+                        });
+                      },
                       child: Row(
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
                           Icon(
-                              Icons.info_outline
+                              Icons.location_on_outlined
                           ),
-                          Text(' Info')
+                          Text('Local')
                         ],
                       ),
+                      color: Colors.lightBlue[200],
+                      height: 50.0,
                     ),
-                    color: Colors.black26,
-                    height: 50.0,
-                  ),
-                ],
+                    SizedBox(width: 15.0,),
+                    // ignore: deprecated_member_use
+                    FlatButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, '/global', arguments: {
+                          'lastUpdateTime' : data['lastUpdateTime'],
+                          'globalNewCases' : data['globalNewCases'],
+                          'globalTotalCases' : data['globalTotalCases'],
+                          'globalNewDeath' : data['globalNewDeath'],
+                          'globalDeath' : data['globalDeath'],
+                          'globalRecovered' : data['globalRecovered']
+                        });
+                      },
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
+                              Icons.location_on_outlined
+                          ),
+                          Text('Global')
+                        ],
+                      ),
+                      color: Colors.redAccent[200],
+                      height: 50.0,
+                    ),
+                    SizedBox(width: 15.0,),
+                    FlatButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, '/info');
+                      },
+                      child: Center(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(
+                                Icons.info_outline
+                            ),
+                            Text(' Info')
+                          ],
+                        ),
+                      ),
+                      color: Colors.black26,
+                      height: 50.0,
+                    ),
+                  ],
+                ),
               ),
             ),
-          ),
-          SizedBox(height: 50.0,),
-          Text(
-            'Developer : inforviraj@gmail.com',
-            style: TextStyle(
-              fontSize: 12.0,
+            SizedBox(height: 50.0,),
+            Text(
+              'Developer : inforviraj@gmail.com',
+              style: TextStyle(
+                fontSize: 12.0,
+              ),
             ),
-          ),
-          SizedBox(height: 12.0,),
-          FloatingActionButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/load');
-              },
-            child: Icon(
-              Icons.refresh_outlined
-            ),
-          )
-        ],
+            SizedBox(height: 12.0,),
+            FloatingActionButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, '/load');
+                },
+              child: Icon(
+                Icons.refresh_outlined
+              ),
+            )
+          ],
       ),
+        ),
       ),
     );
   }
--- lib/pages/information.dart
+++ lib/pages/information.dart
@@ -10,12 +10,70 @@ class _InfoPageState extends State<InfoPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text(''),
+      ),
         body: SafeArea(
-          child: Center(
+          child: SingleChildScrollView(
             child: Container(
-              child: Text('- Under Development -'),
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(20.0,20.0,20.0,0),
+                    child: Container(
+                      child: Column(
+                        children: [
+                          Text(
+                              'What is Covid-19 ?',
+                              style: TextStyle(
+                                fontWeight: FontWeight.w600,
+                                fontSize: 18,
+                              )
+                          ),
+                          SizedBox(height: 10.0),
+                          Text(
+                            'Coronavirus disease (COVID-19) is an infectious disease caused by a newly discovered coronavirus. Most people infected with the COVID-19 virus will experience mild to moderate respiratory illness and recover without requiring special treatment. Older people, and those with underlying medical problems like cardiovascular disease, diabetes, chronic respiratory disease, and cancer are more likely to develop serious illness. The best way to prevent and slow down transmission is to be well informed about the COVID-19 virus, the disease it causes and how it spreads. Protect yourself and others from infection by washing your hands or using an alcohol based rub frequently and not touching your face. The COVID-19 virus spreads primarily through droplets of saliva or discharge from the nose when an infected person coughs or sneezes, so it’s important that you also practice respiratory etiquette (for example, by coughing into a flexed elbow).',
+                            style: TextStyle(
+                                fontWeight: FontWeight.w300,
+                                fontSize: 16
+                            ),
+                            textAlign: TextAlign.justify,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 10.0),
+                  Text(
+                      'Source',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w600,
+                        fontSize: 18,
+                      )
+                  ),
+                  SizedBox(height: 10.0),
+                  Image(
+                    image: AssetImage('assets/hpb.png'),
+                    width: 350,
+                    height: 80,
+                  ),
+                  SizedBox(height: 10.0),
+                  // Text(
+                  //     'Developer',
+                  //     style: TextStyle(
+                  //       fontWeight: FontWeight.w600,
+                  //       fontSize: 18,
+                  //     )
+                  // ),
+                  Image(
+                    image: AssetImage('assets/dev.png'),
+                    width: 350,
+                    height: 80,
+                  ),
+                ],
+              ),
             ),
-          ),
+          )
         )
     );
   }
